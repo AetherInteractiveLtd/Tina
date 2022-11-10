2c543847fce5ec4620469220160a1065006f99fe
RigidOfficial <dneim@protonmail.com>
2022-11-10 17:46:24 +0400

    fix: fix action

--- tests/src/tests/conditions.spec.ts
+++ tests/src/tests/conditions.spec.ts
@@ -5,11 +5,11 @@ import { X } from "@rbxts/tina";
 export = () => {
 	describe("Conditional methods", () => {
 		it("should check evaluations correctness", () => {
-			expect(X.eval(X.and(true, false))).to.be.equal(false);
-			expect(X.eval(X.xor(false, false))).to.be.equal(false);
+			expect(X.EVAL(X.AND(true, false))).to.be.equal(false);
+			expect(X.EVAL(X.XOR(false, false))).to.be.equal(false);
 
-			expect(X.eval(() => 5 > 3)).to.be.equal(true);
-			expect(X.eval(false)).never.be.equal(true);
+			expect(X.EVAL(() => 5 > 3)).to.be.equal(true);
+			expect(X.EVAL(false)).never.be.equal(true);
 		});
 	});
 };
