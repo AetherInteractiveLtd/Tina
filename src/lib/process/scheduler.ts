bbff823a11fc6beee1f60da2e2077a419d6d73f6
Adam Drake <adam-drake1@hotmail.co.uk>
2022-11-24 14:20:21 +0000

    Converted processes map to an array. Added processToRemove as elements of the array to remove after update

--- src/lib/process/scheduler.ts
+++ src/lib/process/scheduler.ts
@@ -5,13 +5,15 @@ const RunService = game.GetService("RunService");
 /**
  *
  */
-class ProcessScheduler {
+export class ProcessScheduler {
 	static TPS = 20; // Grab value from tina.yaml when able
 	private lastTick: number;
 	private timeBetweenTicks: number;
-	private isStarted = false;
 	private connection?: RBXScriptConnection;
-	private processes = new Map<string, Process>();
+	private processes: Array<Process> = new Array();
+	private processesToRemove: Array<Process> = new Array();
+	/** Used to keep track of when the .update() method is running since .removeProcess() could be called during a Process callback. */
+	private updating = false;
 
 	constructor() {
 		this.timeBetweenTicks = 1 / ProcessScheduler.TPS;
@@ -29,7 +31,9 @@ class ProcessScheduler {
 	}
 
 	private update(): void {
-		for (const [_, process] of pairs(this.processes)) {
+		this.updating = true;
+
+		for (const process of this.processes) {
 			// Update suspended processes
 			if (process.isSuspended) {
 				process.suspensionTime -= 1;
@@ -43,30 +47,48 @@ class ProcessScheduler {
 				process.call();
 			}
 		}
-	}
 
-	public removeProcess(process: Process): void {
-		this.processes.delete(process.name);
-
-		// Check if there are still processes to run
-		if (this.processes.size() === 0) {
-			this.stop();
+		// Remove processes in queue
+		for (const process of this.processesToRemove) {
+			this._removeProcess(process);
 		}
+		this.processesToRemove = new Array();
+
+		this.updating = false;
 	}
 
 	public addProcess(process: Process): void {
-		if (!this.processes.has(process.name)) {
-			this.processes.set(process.name, process);
+		if (!this.processes.includes(process)) {
+			this.processes.push(process);
 			this.start();
 		}
 	}
 
+	public removeProcess(process: Process): void {
+		// If Processes are currently being updated then deffer removal until updating has finished
+		if (this.updating) {
+			this.processesToRemove.push(process);
+		} else {
+			this._removeProcess(process);
+		}
+	}
+
+	private _removeProcess(process: Process): void {
+		const index = this.processes.indexOf(process);
+		if (index >= 0) {
+			this.processes.remove(index);
+			if (this.processes.size() === 0) {
+				this.stop();
+			}
+		}
+	}
+
 	public hasProcess(name: string): boolean {
-		return this.processes.has(name);
+		return !!this.processes.find((p) => p.name === name);
 	}
 
 	public getProcess(name: string): Process | undefined {
-		return this.processes.get(name);
+		return this.processes.find((p) => p.name === name);
 	}
 
 	private start(): void {
