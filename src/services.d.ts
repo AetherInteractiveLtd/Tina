73db8380922a650f896754ca8e95f4681c634119
Christopher Buss <christopher.buss+github@protonmail.com>
2022-12-06 18:05:10 +0000

    refactor: remove hard-coded testez path

--- src/services.d.ts
+++ src/services.d.ts
@@ -1,7 +1,3 @@
-interface ReplicatedStorage extends Instance {
-	include: { node_modules: { "@rbxts": { testez: { src: ModuleScript } } } };
-}
-
 interface ServerScriptService extends Instance {
 	Package: Folder;
 }
