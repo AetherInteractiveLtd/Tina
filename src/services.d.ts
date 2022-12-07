cacc1fcc295d839d47289d1b7adcf2fb5333c311
Adam Drake <adam-drake1@hotmail.co.uk>
2022-12-07 11:05:40 +0000

    Moved package in studio to ReplicatedStorage so Roact components are accessible by client

--- src/services.d.ts
+++ src/services.d.ts
@@ -1,7 +1,4 @@
 interface ReplicatedStorage extends Instance {
 	include: { node_modules: { "@rbxts": { testez: { src: ModuleScript } } } };
-}
-
-interface ServerScriptService extends Instance {
 	Package: Folder;
 }
