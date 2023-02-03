5d9183421aa66c30fefff0dbe9e90ec13ede0b16
Adam Drake <adam-drake1@hotmail.co.uk>
2023-02-03 02:09:19 +0000

    moved Package to ReplicatedStorage so it can be accessed on client and server

--- src/testBootstrap.server.ts
+++ src/testBootstrap.server.ts
@@ -1,4 +1,10 @@
 import { ReplicatedStorage } from "@rbxts/services";
 import TestEZ from "@rbxts/testez";
 
+<<<<<<< HEAD
 TestEZ.TestBootstrap.run([ReplicatedStorage.Package]); // Run tests
+=======
+const { TestBootstrap } = require(game.GetService("ReplicatedStorage").include.node_modules["@rbxts"].testez
+	.src) as TestEZ;
+TestBootstrap.run([game.GetService("ReplicatedStorage").Package]); // Run tests
+>>>>>>> 4b317f2 (moved Package to ReplicatedStorage so it can be accessed on client and server)
