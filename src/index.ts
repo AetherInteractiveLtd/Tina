9a1b10a24ad7a40abed244dddc08ee22c3836403
Christopher Buss <32301681+christopher-buss@users.noreply.github.com>
2023-03-16 11:31:58 +0000

    fix(ecs): incorrect type in flyweight (#53)

--- src/index.ts
+++ src/index.ts
@@ -214,7 +214,7 @@ namespace Tina {
 	 *
 	 * @returns A flyweight component.
 	 */
-	export function createFlyweight<T extends Tree<Type>>(schema: T): Flyweight<T> {
+	export function createFlyweight<T extends object>(schema: T): Flyweight<T> {
 		return ComponentInternalCreation.createFlyweight(schema);
 	}
 }
