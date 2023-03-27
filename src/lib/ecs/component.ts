ed27edafb9263ea1088e4d4bba1e3f19aa14ec62
Christopher Buss <christopher.buss+github@protonmail.com>
2023-03-27 15:41:23 +0100

    docs: update documentation

--- src/lib/ecs/component.ts
+++ src/lib/ecs/component.ts
@@ -28,8 +28,35 @@ export type Component<T extends Tree<Type>> = Mutable<ComponentData<T>> & {
 	 * will be given to the entity.
 	 */
 	defaults?: Partial<OptionalKeys<T>>;
+	/**
+	 * Clones all the data from one entity to another. This will
+	 * overwrite any existing data for the target entity. If you want
+	 * to copy specific properties, then you should do this manually.
+	 *
+	 * @param fromEntityId The entity to copy from.
+	 * @param toEntityId The entity to copy to.
+	 */
 	clone(fromEntityId: EntityId, toEntityId: EntityId): void;
+	/**
+	 * Resets the data for the given entity to the default values if
+	 * they are defined.
+	 *
+	 * @param entityId The entity to reset.
+	 */
 	reset(entityId: EntityId): void;
+	/**
+	 * Sets the data for the given entity.
+	 *
+	 * The set function is used to update any observers that are
+	 * watching the given component. If there are no observers, then it
+	 * is recommended to use the component data directly.
+	 *
+	 * There is no equality check, so it is recommended to only use this
+	 * function when the data has changed.
+	 *
+	 * @param entityId The entity to update.
+	 * @param data The data to update.
+	 */
 	set<U extends Partial<OptionalKeys<T>>>(entityId: EntityId, data: U): void;
 };
 
@@ -139,6 +166,14 @@ export namespace ComponentInternalCreation {
 			defaults: undefined,
 			observers: observers,
 
+			/**
+			 * Clones all the data from one entity to another. This will
+			 * overwrite any existing data for the target entity. If you want
+			 * to copy specific properties, then you should do this manually.
+			 *
+			 * @param fromEntityId The entity to copy from.
+			 * @param toEntityId The entity to copy to.
+			 */
 			clone(fromEntityId: EntityId, toEntityId: EntityId): void {
 				// eslint-disable-next-line roblox-ts/no-array-pairs
 				for (const [key] of pairs(componentData)) {
@@ -150,6 +185,8 @@ export namespace ComponentInternalCreation {
 			/**
 			 * Resets the data for the given entity to the default values if
 			 * they are defined.
+			 *
+			 * @param entityId The entity to reset.
 			 */
 			reset(entityId: EntityId): void {
 				if (this.defaults === undefined) {
